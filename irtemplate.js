1dadc947c01b115961143fe291131df3402badb6
aborovsky <kycok.6o6pa@gmail.com>
2015-10-06 13:48:14 +0300

    Great improvements

--- irtemplate.js
+++ irtemplate.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013, 2015 IBM Corp.
+ * Copyright 2013 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,77 +14,95 @@
  * limitations under the License.
  **/
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     "use strict";
-    var cron = require("cron");
 
-    function InjectNode(n) {
-        RED.nodes.createNode(this,n);
-        this.topic = n.topic;
-        this.payload = n.payload;
-        this.payloadType = n.payloadType;
-        this.repeat = n.repeat;
-        this.crontab = n.crontab;
-        this.once = n.once;
+    var SPLIT_REGEX = /[.,:;]/;
+
+    function IRTemplateNode(n) {
+        this.ircommands = n.ircommands || [];
+        this.ircommandsWithAliases = [];
+        this.outputs = this.ircommands.length;
+        RED.nodes.createNode(this, n);
         var node = this;
-        this.interval_id = null;
-        this.cronjob = null;
 
-        if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-            this.repeat = this.repeat * 1000;
-            if (RED.settings.verbose) { this.log(RED._("irtemplate.repeat",this)); }
-            this.interval_id = setInterval( function() {
-                node.emit("input",{});
-            }, this.repeat );
-        } else if (this.crontab) {
-            if (RED.settings.verbose) { this.log(RED._("irtemplate.crontab",this)); }
-            this.cronjob = new cron.CronJob(this.crontab,
-                function() {
-                    node.emit("input",{});
-                },
-                null,true);
-        }
-
-        if (this.once) {
-            setTimeout( function(){ node.emit("input",{}); }, 100);
-        }
-
-        this.on("input",function(msg) {
-            var msg = {topic:this.topic};
-            if ( (this.payloadType == null && this.payload === "") || this.payloadType === "date") {
-                msg.payload = Date.now();
-            } else if (this.payloadType == null || this.payloadType === "string") {
-                msg.payload = this.payload;
-            } else {
-                msg.payload = "";
+        for (var key in this.ircommands) {
+            var aliases = this.ircommands[key].name.split(SPLIT_REGEX);
+            if (aliases.length <= 1) {
+                this.ircommandsWithAliases.push({
+                    name: this.ircommands[key].name,
+                    value: this.ircommands[key].value,
+                    output: key
+                });
+                continue;
             }
-            this.send(msg);
-            msg = null;
+            for (var aliasKey in aliases)
+                this.ircommandsWithAliases.push({
+                    name: aliases[aliasKey].trim(),
+                    value: this.ircommands[key].value,
+                    output: key
+                });
+        }
+
+        this.on('input', function (msg) {
+            var toSend;
+            var output;
+            var ircommand;
+            if (typeof(msg) === "object" && msg.cmd && msg.cmd === ('test_ir#' + node.id.toString())) {
+                output = parseInt(msg.output);
+                ircommand = msg.ircommand;
+            }
+            else {
+                debugger;
+                if (!(msg && msg.hasOwnProperty('payload'))) return;
+                var payload = msg.payload;
+                if (typeof(msg.payload) === "object") {
+                    payload = msg.payload;
+                } else if (typeof(msg.payload) === "string") {
+                    try {
+                        payload = JSON.parse(msg.payload);
+                    } catch (e) {
+                        payload = {cmd: msg.payload.toString()};
+                    }
+                }
+                if (payload == null)
+                    return node.error('IRTemplate: msg.payload required!');
+
+                for (var key in node.ircommandsWithAliases)
+                    if (node.ircommandsWithAliases[key].name === payload.cmd) {
+                        output = parseInt(node.ircommandsWithAliases[key].output) + 1;
+                        ircommand = node.ircommandsWithAliases[key].value;
+                        break;
+                    }
+                if (!output || !ircommand)
+                    return node.error('IRTemplate: Unknown command[' + payload.cmd + ']');
+            }
+            toSend = new Array(output);
+            toSend[output - 1] = {payload: ircommand.toString()};
+            return node.send(toSend);
         });
     }
 
-    RED.nodes.registerType("irtemplate",IRTemplateNode);
+    RED.nodes.registerType("irtemplate", IRTemplateNode);
 
-    IRTemplateNode.prototype.close = function() {
-        if (this.interval_id != null) {
-            clearInterval(this.interval_id);
-            if (RED.settings.verbose) { this.log(RED._("irtemplate.stopped")); }
-        } else if (this.cronjob != null) {
-            this.cronjob.stop();
-            if (RED.settings.verbose) { this.log(RED._("irtemplate.stopped")); }
-            delete this.cronjob;
-        }
-    }
-
-    RED.httpAdmin.post("/irtemplate/:id", RED.auth.needsPermission("irtemplate.write"), function(req,res) {
+    RED.httpAdmin.post("/irtemplate/:id/:ircommand/:output", RED.auth.needsPermission("irtemplate.write"), function (req, res) {
         var node = RED.nodes.getNode(req.params.id);
-        if (node != null) {
+        var ircommand = req.params.ircommand ? req.params.ircommand.toString() : null;
+        var output = parseInt(req.params.output);
+        var msg;
+        if (ircommand !== '' && typeof(ircommand) !== 'undefined')
+            msg = {
+                cmd: 'test_ir#' + node.id.toString(),
+                ircommand: ircommand,
+                output: output
+            }
+        if (node != null && msg != null) {
             try {
-                node.receive();
+                node.receive(msg);
                 res.send(200);
-            } catch(err) {
+            } catch (err) {
                 res.send(500);
-                node.error(RED._("irtemplate.failed",{error:err.toString()}));
+                node.error(RED._("irtemplate.failed", {error: err.toString()}));
             }
         } else {
             res.send(404);
